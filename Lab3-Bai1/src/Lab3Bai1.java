03b6502986ee41e61f87bd59c7095fcb824f6428
NgTienDat <dat.nti.64cntt@ntu.edu.vn>
2025-02-21 16:35:32 +0700

    Lab3-Bai1

---
+++ Lab3-Bai1/src/Lab3Bai1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Lab3Bai1 {
+
+	public static void main(String[] args) {
+		Scanner scanner= new Scanner(System.in);
+		System.out.print("Nhập một số nguyên:");
+		int N=scanner.nextInt();
+		scanner.close();
+		boolean ok=true;
+		if(N<2) {
+			ok=false;
+		}else {
+			for(int i=2;i<Math.sqrt(N);i++) {
+				if(N%i==0) {
+					ok=false;
+					break;
+				}
+			}
+		}
+		if(ok) {
+			   System.out.println(N + " là số nguyên tố.");
+		}else {
+			System.out.println(N + " không phải là số nguyên tố.");
+			
+		}
+		
+	}
+
+}
